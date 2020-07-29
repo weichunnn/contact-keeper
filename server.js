484b5ab486720902db93ae59641a7a53dec09b1c
WeiChunnn <tanweichun@outlook.com>
2020-07-30 01:43:38 +0800

    Added dotenv, middleware and connection to DB

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
+require('dotenv').config({ path: './config/config.env' });
+const colors = require('colors');
+
+const connectDb = require('./config/db');
 
 const app = express();
+
+// Middleware
+app.use(express.json({ extended: false }));
+
+// Connect with Database
+connectDb();
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to ContactKeeper' });
@@ -13,4 +23,10 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(
+  PORT,
+  console.log(
+    `Server running smoothly in ${process.env.NODE_ENV} mode on port: ${PORT}`
+      .yellow.bold
+  )
+);
