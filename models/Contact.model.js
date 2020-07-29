4c455b2f41a47b0329be6e29bf71ad38ae9855bc
WeiChunnn <tanweichun@outlook.com>
2020-07-30 02:50:11 +0800

    Updated Contact Schema

---
+++ models/Contact.model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ContactSchema = new Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  phone: {
+    type: String
+  },
+  type: {
+    type: String,
+    default: 'personal'
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+module.exports = mongoose.model('Contact', ContactSchema);
